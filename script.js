2cf0546dd2fe78a33bec7f4ba7e0350b08f16fbf
Daniel <danielmacnog@gmail.com>
2022-06-06 14:35:16 -0300

    committing trump cardgame

---
+++ script.js
@@ -0,0 +1,390 @@
+// Obs.: Aqui temos um objeto "dentro" de outro objeto --->
+// Super Trunfo de Stranger Things ( total: 14 cartas; 7 do usuário X 7 do computador ).
+
+var carta1 = {
+    nome: "Demogorgon",
+    imagem:
+      "https://i.pinimg.com/736x/ea/d0/ba/ead0ba1628ebf206b8cdb5efaafd951c.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 8,
+      inteligência: 5
+    }
+  };
+  
+  // console.log(carta1.atributos.ataque); --> exemplo
+  
+  var carta2 = {
+    nome: "Eleven",
+    imagem:
+      "https://i.pinimg.com/originals/9d/8e/d2/9d8ed26aa8861dbdbaee70d87de4951d.jpg",
+    atributos: {
+      ataque: 10,
+      defesa: 6,
+      inteligência: 8
+    }
+  };
+  
+  var carta3 = {
+    nome: "Jim Hopper",
+    imagem:
+      "https://i.pinimg.com/736x/2b/7a/ee/2b7aeeaa6e76179cf20c8e1dbb867d4f.jpg",
+    atributos: {
+      ataque: 9,
+      defesa: 8,
+      inteligência: 6
+    }
+  };
+  
+  var carta4 = {
+    nome: "Joyce Byers",
+    imagem:
+      "https://pm1.narvii.com/6650/6705e2cf7e5f270ce76f391e3865f6cc72175254_hq.jpg",
+    atributos: {
+      ataque: 5,
+      defesa: 7,
+      inteligência: 9
+    }
+  };
+  
+  var carta5 = {
+    nome: "Will",
+    imagem:
+      "https://tv-fanatic-res.cloudinary.com/iu/s--7v1eRaK8--/t_full/cs_srgb,f_auto,fl_strip_profile.lossy,q_auto:420/v1562255050/will-plays-d-and-d-stranger-things-s3e3.png",
+    atributos: {
+      ataque: 6,
+      defesa: 4,
+      inteligência: 8
+    }
+  };
+  
+  var carta6 = {
+    nome: "Lucas",
+    imagem:
+      "https://th.bing.com/th/id/OIP.sWtzR-1p22wdgNFIKYTzuwHaJ3?pid=ImgDet&rs=1.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 7,
+      inteligência: 6
+    }
+  };
+  
+  var carta7 = {
+    nome: "Dustin",
+    imagem:
+      "https://th.bing.com/th/id/OIP.k5WSaQpkMSYoYis4NJApKgHaJR?pid=ImgDet&rs=1.jpg",
+    atributos: {
+      ataque: 6,
+      defesa: 4,
+      inteligência: 9
+    }
+  };
+  
+  var carta8 = {
+    nome: "Robin Buckley",
+    imagem:
+      "https://i.pinimg.com/736x/83/7c/95/837c953363286943e4acb228482b7753.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 6,
+      inteligência: 9
+    }
+  };
+  
+  var carta9 = {
+    nome: "Mad Max",
+    imagem: "https://wallpapercave.com/wp/wp3894383.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 7,
+      inteligência: 7
+    }
+  };
+  
+  var carta10 = {
+    nome: "Mike",
+  
+    imagem:
+      "https://i.pinimg.com/originals/0b/86/c4/0b86c4981e07ff6ed7e2c25fa90b7bb9.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 5,
+      inteligência: 9
+    }
+  };
+  
+  var carta11 = {
+    nome: "Nancy",
+    imagem:
+      "https://i.pinimg.com/736x/66/12/bc/6612bc38073bfff04d967b86371ef96d.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 7,
+      inteligência: 8
+    }
+  };
+  
+  var carta12 = {
+    nome: "Jonathan",
+    imagem:
+      "https://th.bing.com/th/id/OIP.yVChbCEb5Cr7W9WJRxCMWwHaLH?pid=ImgDet&rs=1.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      inteligência: 8
+    }
+  };
+  
+  var carta13 = {
+    nome: "Steve",
+    imagem:
+      "https://th.bing.com/th/id/R.f6f1d8a487f6fcefcb3e57046a0eff8b?rik=Qizv%2fAnzTpOFnw&pid=ImgRaw&r=0.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 8,
+      inteligência: 4
+    }
+  };
+  
+  var carta14 = {
+    nome: "Devorador de Mentes",
+    imagem:
+      "https://th.bing.com/th/id/R.1de813e8d321f577e89344ae7c17d245?rik=d50Lmip8Ha9ZjA&pid=ImgRaw&r=0.jpg",
+    atributos: {
+      ataque: 10,
+      defesa: 8,
+      inteligência: 9
+    }
+  };
+  
+  var carta15 = {
+    nome: "Vecna",
+    imagem:
+      "https://criticalhits.com.br/wp-content/uploads/2022/05/stranger-things-vecna.jpg",
+    atributos: {
+      ataque: 10,
+      defesa: 8,
+      inteligência: 10
+    }
+  };
+  
+  var carta16 = {
+    nome: "Argyle",
+    imagem:
+      "https://media1.popsugar-assets.com/files/thumbor/ZERBEjL0bksSAJhvvwPQ6g3paOY/1749x51:3251x1553/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2022/05/27/671/n/1922283/3c30f0da6290e9178d5e76.10390608_/i/stranger-things-argyle-actor.jpg",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      inteligência: 5
+    }
+  };
+
+  var carta17 = {
+    nome: "Murray",
+    imagem:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mh-strangerthingsmurray-1563208137.jpg?crop=0.500xw:1.00xh;0.348xw,0&resize=480:*",
+    atributos: {
+      ataque: 7,
+      defesa: 7,
+      inteligência: 9
+    }
+  };
+
+  var carta18 = {
+    nome: "Erica",
+    imagem:
+      "https://i.pinimg.com/736x/85/7f/ca/857fca9a888c575c61b95930dd91377f.jpg",
+    atributos: {
+      ataque: 6,
+      defesa: 7,
+      inteligência: 8
+    }
+  };
+
+  var carta19 = {
+    nome: "Eddie",
+    imagem:
+      "https://cdn.mos.cms.futurecdn.net/ghhbWhj2JN8aYxnkaXr75b.jpg",
+    atributos: {
+      ataque: 8,
+      defesa: 7,
+      inteligência: 8
+    }
+  };
+
+  var carta20 = {
+    nome: "Chrissy",
+    imagem:
+      "https://pbs.twimg.com/media/FTyKgghWIAIB3NF?format=jpg&name=small",
+    atributos: {
+      ataque: 6,
+      defesa: 6,
+      inteligência: 8
+    }
+  };
+
+  var cartas = [
+    carta1,
+    carta2,
+    carta3,
+    carta4,
+    carta5,
+    carta7,
+    carta8,
+    carta9,
+    carta10,
+    carta11,
+    carta12, 
+    carta13,
+    carta14,
+    carta15,
+    carta16, 
+    carta17, 
+    carta18,
+    carta19,
+    carta20,
+
+  ];
+  
+  var cartaMaquina;
+  var cartaJogador;
+  
+  function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 20);
+  
+    cartaMaquina = cartas[numeroCartaMaquina];
+  
+    // console.log(cartaMaquina);
+  
+    var numeroCartaJogador = parseInt(Math.random() * 20);
+  
+    while (numeroCartaMaquina == numeroCartaJogador) {
+      numeroCartaJogador = parseInt(Math.random() * 20);
+    }
+  
+    cartaJogador = cartas[numeroCartaJogador];
+  
+    console.log(cartaJogador);
+  
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = false;
+  
+    // exibirOpcoes();
+    exibirCartaJogador();
+  }
+  
+  /* function exibirOpcoes() {
+    var opcoes = document.getElementById("opcoes");
+    var opcoesTexto = "";
+  
+    for (var atributo in cartaJogador.atributos) {
+      opcoesTexto +=
+        "<input type='radio' name='atributo' value='" +
+        atributo +
+        "'>" +
+        atributo;
+    }
+    opcoes.innerHTML = opcoesTexto;
+  } */
+  
+  function obtemAtributoSelecionado() {
+    var radioAtributos = document.getElementsByName("atributo");
+  
+    for (var i = 0; i < radioAtributos.length; i++) {
+      if (radioAtributos[i].checked == true) {
+        return radioAtributos[i].value;
+      }
+    }
+  }
+  
+  function jogar() {
+    var divResultado = document.getElementById("resultado");
+  
+    var atributoSelecionado = obtemAtributoSelecionado();
+  
+    if (typeof atributoSelecionado === "undefined") {
+      htmlResultado =
+        "<p class='resultado-final'>Você deve escolher um atributo</p>";
+    } else {
+      var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+      var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+  
+      if (valorCartaJogador > valorCartaMaquina) {
+        htmlResultado = "<p class='resultado-final'>Venceu</p>";
+      } else if (valorCartaMaquina > valorCartaJogador) {
+        htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+      } else {
+        valorCartaJogador == valorCartaMaquina;
+        htmlResultado = "<p class='resultado-final'>Empatou</p>";
+      }
+    }
+    // console.log(atributoSelecionado);
+    // console.log(cartaMaquina);
+  
+    divResultado.innerHTML = htmlResultado;
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnSortear").disabled = false;
+    exibirCartaMaquina();
+  }
+  
+  //console.log(atributoSelecionado);
+  //console.log(cartaJogador.atributos[atributoSelecionado]);
+  
+  function exibirCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador");
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  
+    /* divCartaJogador.style.backgroundImage =" url(" + cartaJogador.imagem + ")"
+    Obs.: Eu poderia fazer dessa forma também ao invés de `url(${cartaJogador.imagem})` */
+  
+    var moldura =
+      '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  
+    var tagHTML = "<div id='opcoes' class='carta-status'>";
+  
+    var opcoesTexto = "";
+  
+    for (var atributo in cartaJogador.atributos) {
+      opcoesTexto +=
+        "<input type='radio' name='atributo' value='" +
+        atributo +
+        "'>" +
+        atributo +
+        " " +
+        cartaJogador.atributos[atributo] +
+        "<br>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  
+    divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div";
+  }
+  
+  function exibirCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina");
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  
+    /* divCartaJogador.style.backgroundImage =" url(" + cartaJogador.imagem + ")"
+    Obs.: Eu poderia fazer dessa forma também ao invés de `url(${cartaJogador.imagem})` */
+  
+    var moldura =
+      '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  
+    var tagHTML = "<div id='opcoes' class='carta-status'>";
+  
+    var opcoesTexto = "";
+  
+    for (var atributo in cartaMaquina.atributos) {
+      opcoesTexto +=
+        "<p input type='text' name='atributo' value='" +
+        atributo +
+        "'>" +
+        atributo +
+        " " +
+        cartaMaquina.atributos[atributo] +
+        "</p>";
+    }
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  
+    divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div";
+  }
+  
